c3ef4983e5358e1376631071101b69ce0932980d
v.abakumov <ava.ossystem@gmail.com>
2022-04-17 18:51:49 +0300

    aws created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
-import { UserModule } from 'src/modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { DbModule } from './shared/db/db.module';
 
 @Module({
-  imports: [
-    DbModule,
-    UserModule,
-    AuthModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
